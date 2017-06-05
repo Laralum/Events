629f8fa6e4c4bc93321bad93e8a1597d364a7f7f
24aitor <contact@aitorriba.com>
2017-06-05 21:33:17 +0200

    Added events trait

---
+++ src/Traits/Events.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Laralum\Roles\Traits;
+
+use Laralum\Events\Models\Event;
+
+trait Events {
+    public function joinedEvents() {
+        return $this->belongsToMany(Event::class, 'laralum_event_user')->withPivot('responsible');
+    }
+
+    public function responsibleEvents () {
+        return $this->joinedEvents()->wherePivot('responsible', true)->get();
+    }
+}
